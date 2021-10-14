05bb49ca69c1f637eaef0adc90099999b724855c
TomekMazurek <37124822+TomekMazurek@users.noreply.github.com>
2021-10-14 15:48:06 +0200

    Add Swagger API documentation (#6)
    add Swagger dependency to pom.xml
    add Swagger Docket basic configuration to PetStoreConfiguration

--- src/main/java/com/github/tomekmazurek/petstore/config/PetStoreConfiguration.java
+++ src/main/java/com/github/tomekmazurek/petstore/config/PetStoreConfiguration.java
@@ -2,15 +2,30 @@ package com.github.tomekmazurek.petstore.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.github.tomekmazurek.petstore"})
 @EntityScan("com.github.tomekmazurek.petstore.model")
 @EnableJpaRepositories("com.github.tomekmazurek.petstore.repository")
+@EnableSwagger2
 public class PetStoreConfiguration {
 
+    @Bean
+    public Docket get() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.github.tomekmazurek.petstore"))
+                .build();
+    }
 }
